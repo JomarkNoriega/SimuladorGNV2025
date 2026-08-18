b82a420e72bac17f074427282404b415eeb6ed67
JomarkNoriega <39424364+JomarkNoriega@users.noreply.github.com>
2026-08-18 16:09:11 -0500

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Si publicas en GitHub Pages, descomenta y pon tu repo:
-// export default defineConfig({ base: "/NOMBRE_DEL_REPO/", plugins: [react()] });
-export default defineConfig({
-  plugins: [react()]
-});
